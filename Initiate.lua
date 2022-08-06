a5b9ded16e3c36b2bde4988cdc9e0226d4938124
spdmshit <109247099+spdmshit@users.noreply.github.com>
2022-08-05 23:13:04 -0500

    Update Initiate.lua

--- Initiate.lua
+++ Initiate.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vodxn/sape/main/MainScript.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/spdmshit/FDP-Client/main/MainScript.lua"))()
